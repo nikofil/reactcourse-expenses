24860ba85c0a5d2cb1f13a1ddc5e922d87f535fd
Nikos Filippakis <aesmade@gmail.com>
2018-06-30 19:55:50 +0300

    Project scaffolding
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+const public = path.join(__dirname, 'public/')
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: public,
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node-modules/,
+        }, {
+            test: /\.s?css/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devServer: {
+        contentBase: public
+    },
+    devtool: 'cheap-module-eval-source-map',
+    mode: 'development',
+}

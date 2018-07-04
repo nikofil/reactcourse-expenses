8db3d0007715abf6cdb068baa8a0b3e152de5ece
Nikos Filippakis <aesmade@gmail.com>
2018-07-04 19:13:50 +0300

    Add params to edit route
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
-const EditExpensePage = () => (
-    <div>Edit</div>
-)
+const EditExpensePage = (props) => {
+    console.log(props)
+    return <div>Editing: { props.match.params.id }</div>
+}
 
 export default EditExpensePage

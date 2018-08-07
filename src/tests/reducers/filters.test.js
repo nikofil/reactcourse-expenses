1e871245489716ec0d067cabb1a21fd5639285f9
Nikos Filippakis <aesmade@gmail.com>
2018-08-07 18:33:59 +0300

    Reducers test
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

---
+++ src/tests/reducers/filters.test.js
@@ -0,0 +1,17 @@
+import moment from 'moment'
+import filtersReducer from '../../reducers/filters'
+
+test('should setup initial state', () => {
+    const state = filtersReducer(undefined, {})
+    expect(state).toEqual({
+        text: '',
+        sortBy: 'date',
+        startDate: moment().startOf('month'),
+        endDate: moment().endOf('month'),
+    })
+})
+
+test('should set sortBy to amount', () => {
+    const state = filtersReducer(undefined, {type: 'SORT_BY', by: 'amount'})
+    expect(state.sortBy).toBe('amount')
+})

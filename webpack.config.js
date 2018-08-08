4a520511a6688b5ac84b7c6391a3901458f10a0e
Nikos Filippakis <aesmade@gmail.com>
2018-08-08 13:13:10 +0300

    Production configuration
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const public = path.join(__dirname, 'public/')
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: public,
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/,
-        }, {
-            test: /\.s?css/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devServer: {
-        contentBase: public,
-        historyApiFallback: true
-    },
-    devtool: 'cheap-module-eval-source-map',
-    mode: 'development',
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    const CSSExtract = new ExtractTextPlugin('styles.css')
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: public,
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+            }, {
+                test: /\.s?css/,
+                use: CSSExtract.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader',
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract,
+        ],
+        devServer: {
+            contentBase: public,
+            historyApiFallback: true,
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        mode: 'development',
+    }
 }

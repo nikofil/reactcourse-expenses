2e4647bcebb50fe2db7f1041c35ed69667b08776
Nikos Filippakis <aesmade@gmail.com>
2018-08-26 12:03:37 +0300

    Test and prod configs for firebase
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 const path = require('path')
+const webpack = require('webpack')
+const dotenv = require('dotenv')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const public = path.join(__dirname, 'public')
 const dist = path.join(public, 'dist')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+if (process.env.NODE_ENV === 'test') {
+    dotenv.config({ path: '.env.test' })
+} else {
+    dotenv.config({ path: '.env.development' })
+}
+
 module.exports = (env) => {
     const isProduction = env === 'production'
     const CSSExtract = new ExtractTextPlugin('styles.css')
@@ -40,6 +51,14 @@ module.exports = (env) => {
         },
         plugins: [
             CSSExtract,
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+            }),
         ],
         devServer: {
             contentBase: public,
